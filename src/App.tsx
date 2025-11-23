7696d8b821a5ed8c6953d7fc3a5b2c7b2bea123d
EladReuveny <126913072+EladReuveny@users.noreply.github.com>
2025-11-23 02:17:45 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import Services from "./pages/Services";
+
+type AppProps = {};
+
+const App = ({}: AppProps) => {
+  return (
+    <div>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
